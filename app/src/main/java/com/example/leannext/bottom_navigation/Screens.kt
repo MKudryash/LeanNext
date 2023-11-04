60030b5acb93c32f00df266d9d9fcb37b85963fb
MKudryash <82795441+MKudryash@users.noreply.github.com>
2023-11-04 22:37:11 +0300

    Layout PassingTest

--- app/src/main/java/com/example/leannext/bottom_navigation/Screens.kt
+++ app/src/main/java/com/example/leannext/bottom_navigation/Screens.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -19,19 +22,25 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.leannext.Models.Criterias
 import com.example.leannext.Models.DevelopmentIndex
 import com.example.leannext.Models.Directions
 import com.example.leannext.R
@@ -125,7 +134,7 @@ fun DiagramScreen() {
             painter = painterResource(id = R.drawable.daigramexample),
             contentDescription = ""
         )
-        Box{
+        Box {
             LazyColumn(
                 modifier = Modifier.fillMaxSize(),
                 contentPadding = PaddingValues(bottom = 92.dp)
@@ -205,7 +214,7 @@ fun DiagramScreen() {
 }
 
 @Composable
-fun TestScreen(onClick:()->Unit) {
+fun TestScreen(onClick: () -> Unit) {
     var search: String by rememberSaveable { mutableStateOf("") }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
@@ -233,12 +242,15 @@ fun TestScreen(onClick:()->Unit) {
                 )
             )
             { index, item ->
+
                 if (index < 9)
 
                     Row(
                         Modifier
                             .fillMaxWidth()
                             .padding(25.dp, 10.dp)
+                            .weight(1f)
+                            .height(85.dp)
                             .clickable(onClick =
                             {
                                 onClick()
@@ -261,14 +273,16 @@ fun TestScreen(onClick:()->Unit) {
                             fontSize = 16.sp,
                             textAlign = TextAlign.Center,
 
-                        )
+                            )
 
                     }
-                Divider(
+                if (index < 8)
+                { Divider(
                     color = MaterialTheme.colorScheme.primary,
                     thickness = 2.dp,
                     modifier = Modifier.padding(15.dp, 10.dp)
                 )
+                }
             }
 
         }
@@ -279,7 +293,11 @@ fun TestScreen(onClick:()->Unit) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CustomSearchView(search: String,   modifier: Modifier = Modifier,onValueChange: (String) -> Unit) {
+fun CustomSearchView(
+    search: String,
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit
+) {
     Box(
         modifier = modifier
             .padding(20.dp)
@@ -300,9 +318,18 @@ fun CustomSearchView(search: String,   modifier: Modifier = Modifier,onValueChan
                 cursorColor = MaterialTheme.colorScheme.onTertiary
             ),
             modifier = Modifier.background(Color(0XFFF5F5F9)),
-            leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "",
-                tint = MaterialTheme.colorScheme.onTertiary) },
-            placeholder = { Text(text = "Искать направление", color = MaterialTheme.colorScheme.onSecondary) }
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search, contentDescription = "",
+                    tint = MaterialTheme.colorScheme.onTertiary
+                )
+            },
+            placeholder = {
+                Text(
+                    text = "Искать направление",
+                    color = MaterialTheme.colorScheme.onSecondary
+                )
+            }
         )
 
     }
@@ -312,41 +339,186 @@ fun CustomSearchView(search: String,   modifier: Modifier = Modifier,onValueChan
 
 @Preview
 @Composable
-fun DirectionTestScreen()
-{
+fun DirectionTestScreen() {
     Column(
         modifier = Modifier
-        .fillMaxSize()
-        .background(MaterialTheme.colorScheme.background),
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
 
-    ){
-        Row {
-            Box(modifier = Modifier
-                .padding(7.dp,20.dp)
-                .clip(RoundedCornerShape(10.dp))
-                .background(Color(0XFFF5F5F9)),
+        ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(7.dp, 25.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(Color(0XFFF5F5F9)),
                 contentAlignment = Alignment.Center
             )
             {
-                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "",
-                    tint = MaterialTheme.colorScheme.onTertiary)
+                Icon(
+                    modifier = Modifier
+                        .padding(7.dp),
+                    imageVector = Icons.Default.ArrowBack, contentDescription = "",
+                    tint = MaterialTheme.colorScheme.onTertiary
+                )
             }
             Text(
                 modifier = Modifier
                     .weight(8f)
-                    .padding(10.dp, 0.dp, 0.dp, 0.dp),
+                    .padding(10.dp, 0.dp, 10.dp, 0.dp),
                 text = "5S и Визуальный менеджмент",
                 color = MaterialTheme.colorScheme.secondary,
                 fontFamily = FontFamily(Font(R.font.neosanspro_medium)),
-                fontSize = 16.sp,
+                fontSize = 20.sp,
                 textAlign = TextAlign.Center,
 
                 )
+        }
+        LazyColumn(
+            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(bottom = 92.dp),
+        )
+        {
+            itemsIndexed(
+                listOf(
+                    Criterias(
+                        1,
+                        "% персонала, ознакомленный с основами, технологиями, инструментами и порядком внедрения системы \"Пять S\"",
+                        1,
+                        ""
+                    ),
+                    Criterias(2, "% участков, обеспеченных плакатами по 5S", 1, ""),
+                    Criterias(
+                        3,
+                        "На земле, на полу, на оборудовании, внутри и под оборудованием, под столами, под стеллажами и т.д. отсутствуют инструменты, оснастка, материалы и прочие предметы",
+                        1,
+                        ""
+                    ),
+                )
+            )
+            { index, item ->
+                var count = index + 1
+                var progressWeight: Float = count.toFloat()
+                var allWeight: Float = 3f - count.toFloat()
+                Column(
 
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    )
+                    {
+                        Text(
+                            text = "${count} вопрос",
+                            fontFamily = FontFamily(Font(R.font.neosanspro_bold)),
+                            fontSize = 24.sp,
+                            color = MaterialTheme.colorScheme.secondary,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.padding(7.dp, 5.dp)
+                        )
+                        Box(
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .padding(5.dp, 0.dp)
+                                .border(1.dp, MaterialTheme.colorScheme.primary, CircleShape)
+                        )
+
+                        {
+                            Text(
+                                text = "i",
+                                fontFamily = FontFamily(Font(R.font.neosanspro_bold)),
+                                fontSize = 18.sp,
+                                color = MaterialTheme.colorScheme.secondary,
+                                modifier = Modifier.padding(12.dp, 3.dp),
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(20.dp, 10.dp,20.dp,20.dp)
+                            .border(2.dp, Color.Transparent, RoundedCornerShape(5.dp))
+                    )
+                    {
+                        Divider(
+                            modifier = Modifier.weight(progressWeight),
+                            color = MaterialTheme.colorScheme.primary, thickness = 6.dp
+                        )
+                        if (allWeight > 0) {
+                            Divider(
+                                modifier = Modifier.weight(allWeight),
+                                color = Color(0xFFD9D9D9), thickness = 6.dp
+                            )
+                        }
+                    }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(15.dp, 0.dp)
+                            .border(
+                                2.dp, Color(0xFFB8C1CC),
+                                RoundedCornerShape(20.dp)
+                            ),
+                        contentAlignment = Alignment.Center
+                    )
+                    {
+                        Text(
+                            text = item.Title,
+                            fontFamily = FontFamily(Font(R.font.neosanspro_bold)),
+                            fontSize = 16.sp,
+                            color = MaterialTheme.colorScheme.secondary,
+                            modifier = Modifier.padding(7.dp),
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                    LazyRow(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(25.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    )
+                    {
+                        itemsIndexed(
+                            listOf(
+                                "1.0", "2.0", "3.0", "4.0", "5.0"
+                            )
+                        )
+                        { index, item ->
+                            Box(
+                                modifier = Modifier.border(
+                                    2.dp, Color(0xFFB8C1CC),
+                                    RoundedCornerShape(20.dp)
+                                )
+                            )
+                            {
+                                Text(
+                                    text = item,
+                                    fontFamily = FontFamily(Font(R.font.neosanspro_regular)),
+                                    fontSize = 16.sp,
+                                    color = MaterialTheme.colorScheme.secondary,
+                                    modifier = Modifier.padding(16.dp)
+                                )
+                            }
+                        }
+                    }
+                    if (index < 2)
+                    { Divider(
+                        color = MaterialTheme.colorScheme.primary,
+                        thickness = 2.dp,
+                        modifier = Modifier.padding(15.dp, 10.dp)
+                    )
+                    }
+                }
+            }
         }
     }
-
 }
+
 @Composable
 fun ProfileScreen() {
     // Column Composable,
