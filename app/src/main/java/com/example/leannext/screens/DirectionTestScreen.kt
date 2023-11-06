df77b5fff0b23fb5f9df4c7f5704b7185dadbd1f
MKudryash <82795441+MKudryash@users.noreply.github.com>
2023-11-06 12:25:06 +0300

    Выбор ответа из вопросов

--- app/src/main/java/com/example/leannext/screens/DirectionTestScreen.kt
+++ app/src/main/java/com/example/leannext/screens/DirectionTestScreen.kt
@@ -2,6 +2,7 @@ package com.example.leannext.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,6 +16,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -25,6 +28,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -43,7 +50,7 @@ import com.example.leannext.R
 fun DirectionTestScreen(
     mainViewModel: MainViewModel = viewModel(factory = MainViewModel.factory),
     navHostController: NavHostController,
-    id:Int,
+    id: Int,
     name: String
 ) {
     val itemsListCriterias =
@@ -54,21 +61,18 @@ fun DirectionTestScreen(
             .background(MaterialTheme.colorScheme.background),
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
+            modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically
         ) {
             Box(
                 modifier = Modifier
                     .padding(7.dp, 25.dp)
                     .clip(RoundedCornerShape(10.dp))
-                    .background(Color(0XFFF5F5F9)),
-                contentAlignment = Alignment.Center
-            )
-            {
+                    .background(Color(0XFFF5F5F9)), contentAlignment = Alignment.Center
+            ) {
                 Icon(
-                    modifier = Modifier
-                        .padding(7.dp),
-                    imageVector = Icons.Default.ArrowBack, contentDescription = "",
+                    modifier = Modifier.padding(7.dp),
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "",
                     tint = MaterialTheme.colorScheme.onTertiary
                 )
             }
@@ -87,8 +91,7 @@ fun DirectionTestScreen(
             verticalArrangement = Arrangement.SpaceEvenly,
             modifier = Modifier.fillMaxSize(),
             contentPadding = PaddingValues(bottom = 92.dp),
-        )
-        {
+        ) {
             items(itemsListCriterias.value) { item ->
                 var count = item.id % 3
                 if (count == 0) count = 3
@@ -99,8 +102,7 @@ fun DirectionTestScreen(
                 ) {
                     Row(
                         verticalAlignment = Alignment.CenterVertically
-                    )
-                    {
+                    ) {
                         Text(
                             text = "${count} вопрос",
                             fontFamily = FontFamily(Font(R.font.neosanspro_bold)),
@@ -132,16 +134,17 @@ fun DirectionTestScreen(
                             .fillMaxWidth()
                             .padding(20.dp, 10.dp, 20.dp, 20.dp)
                             .border(2.dp, Color.Transparent, RoundedCornerShape(5.dp))
-                    )
-                    {
+                    ) {
                         Divider(
                             modifier = Modifier.weight(progressWeight),
-                            color = MaterialTheme.colorScheme.primary, thickness = 6.dp
+                            color = MaterialTheme.colorScheme.primary,
+                            thickness = 6.dp
                         )
                         if (allWeight > 0) {
                             Divider(
                                 modifier = Modifier.weight(allWeight),
-                                color = Color(0xFFD9D9D9), thickness = 6.dp
+                                color = Color(0xFFD9D9D9),
+                                thickness = 6.dp
                             )
                         }
                     }
@@ -150,12 +153,9 @@ fun DirectionTestScreen(
                             .fillMaxWidth()
                             .padding(15.dp, 0.dp)
                             .border(
-                                2.dp, Color(0xFFB8C1CC),
-                                RoundedCornerShape(20.dp)
-                            ),
-                        contentAlignment = Alignment.Center
-                    )
-                    {
+                                2.dp, Color(0xFFB8C1CC), RoundedCornerShape(20.dp)
+                            ), contentAlignment = Alignment.Center
+                    ) {
                         Text(
                             text = item.title,
                             fontFamily = FontFamily(Font(R.font.neosanspro_bold)),
@@ -165,33 +165,40 @@ fun DirectionTestScreen(
                             textAlign = TextAlign.Center
                         )
                     }
+                    val listState = rememberLazyListState()
+                    var selectedIndex by remember { mutableStateOf(-1) }
                     LazyRow(
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(25.dp),
                         horizontalArrangement = Arrangement.SpaceBetween,
                         verticalAlignment = Alignment.CenterVertically
-                    )
-                    {
+                    ) {
                         itemsIndexed(
                             listOf(
                                 "1.0", "2.0", "3.0", "4.0", "5.0"
                             )
-                        )
-                        { index, item ->
+
+                        ) { index, item ->
+                            var borderColor: Color
+                            if (index == selectedIndex) borderColor =MaterialTheme.colorScheme.primary
+                            else borderColor = Color(0xFFB8C1CC)
                             Box(
                                 modifier = Modifier.border(
-                                    2.dp, Color(0xFFB8C1CC),
-                                    RoundedCornerShape(20.dp)
+                                    2.dp, borderColor, RoundedCornerShape(20.dp)
                                 )
-                            )
-                            {
+                            ) {
                                 Text(
                                     text = item,
                                     fontFamily = FontFamily(Font(R.font.neosanspro_regular)),
                                     fontSize = 16.sp,
                                     color = MaterialTheme.colorScheme.secondary,
-                                    modifier = Modifier.padding(16.dp)
+                                    modifier = Modifier
+                                        .padding(16.dp)
+                                        .selectable(selected = index == selectedIndex, onClick = {
+                                            if (selectedIndex != index) selectedIndex =
+                                                index else selectedIndex = -1
+                                        })
                                 )
                             }
                         }
