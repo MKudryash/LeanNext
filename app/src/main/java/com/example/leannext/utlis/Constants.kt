0fb51eff414053d7a69c3b6225577453e11756c1
Julia Mamsheva <82795441+MKudryash@users.noreply.github.com>
2023-11-24 14:52:38 +0300

    Rename bottom item

--- app/src/main/java/com/example/leannext/utlis/Constants.kt
+++ app/src/main/java/com/example/leannext/utlis/Constants.kt
@@ -7,9 +7,9 @@ import com.example.leannext.bottom_navigation.BottomItem
 
 sealed class  Screen(val route:String)
 object Constants {
-    object DiagramScreen : BottomItem("Diagram", R.drawable.diagram,"diagram")
-    object ProfileScreen : BottomItem("Profile", R.drawable.profile,"profile")
-    object TestScreen : BottomItem("Test", R.drawable.test,"test")
+    object DiagramScreen : BottomItem("Диаграмма", R.drawable.diagram,"diagram")
+    object ProfileScreen : BottomItem("Профиль", R.drawable.profile,"profile")
+    object TestScreen : BottomItem("Тесты", R.drawable.test,"test")
     object DirectionTestScreen:Screen("directionTestScreen/{id}/{name}")
     object MainScreen:Screen("mainScreen")
     var userName:String = ""
